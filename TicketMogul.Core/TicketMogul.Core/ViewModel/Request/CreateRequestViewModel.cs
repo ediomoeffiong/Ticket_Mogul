cea3da2e324788324241cc600c5edfe1c99b53ff
Covenant <ediomoemmaeffiong@gmail.com>
2024-06-25 14:35:03 +0100

    First version

---
+++ TicketMogul.Core/TicketMogul.Core/ViewModel/Request/CreateRequestViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketMogul.Core.ViewModel.Request
+{
+    public class CreateRequestViewModel
+    {
+        public string Name { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public string Service { get; set; }
+    }
+}
---
+++ TicketMogul/Controllers/RequestController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TicketMogul.Core.ViewModel.Request;
+
+namespace TicketMogul.Controllers
+{
+    public class RequestController : Controller
+    {
+        private object RequestService { get; set; }
+        public RequestController()
+        {
+
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult CreateRequest(CreateRequestViewModel data)
+        {
+            return Ok();
+        }
+    }
+}
